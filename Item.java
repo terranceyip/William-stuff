5bbb908f9cf0526f630fc6aec1cc886f0bf44b2e
SusanSuY <susan_loves_cheese@hotmail.com>
2015-11-18 17:33:53 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,1 +1,11 @@
+public abstract class Item {
+  
+  public abstract String getInfo(...);
+  public abstract int getPrice(...);
+  
+  protected int price;
+  protected int sNo;
 
+  //Add other fields if necessary
+
+}
