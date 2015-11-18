d9e9257687cbc1ece5e423992ef58f26efbfdb1d
SusanSuY <susan_loves_cheese@hotmail.com>
2015-11-18 17:39:57 -0500

    Update Audio.java

--- Audio.java
+++ Audio.java
@@ -1,1 +1,12 @@
-
+public class Audio extends Item {
+  protected String artistName;
+  public String getInfo(...) {
+    //Returns sNo, Name, Artist name, etc in a string
+  }
+  
+  @Override
+  public int getPrice(...) {
+    //override
+    //...
+  }
+}
